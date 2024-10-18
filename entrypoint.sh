409424c0d1c57d97c175d3603fa828018fb890bf
dw <dw@konakona.moe>
2024-10-18 11:58:09 +0200

    fix: entrypoint check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ ! -d "/flagcounter/config/static/" ]    && mv static-tmp/* /flagcounter/src/static && rm -rf static-tmp
-[ ! -f "/flagcounter/config/config.yml" ] && mv /flagcounter/config.json /flagcounter/config/
+[ ! -d "/flagcounter/config/static/" ]     && mv static-tmp/* /flagcounter/src/static && rm -rf static-tmp
+[ ! -f "/flagcounter/config/config.json" ] && mv /flagcounter/config.json /flagcounter/config/
 
 node src/index.js --docker
