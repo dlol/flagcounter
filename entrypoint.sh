8ce9871638d6d21d124623053404d35b1fc3af31
dw <dw@konakona.moe>
2024-10-18 14:19:12 +0200

    fix(entrypoint): cp -r

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -z "$(ls -A "/flagcounter/src/static")" ] && cp static-tmp/* /flagcounter/src/static
+[ -z "$(ls -A "/flagcounter/src/static")" ] && cp -r static-tmp/* /flagcounter/src/static
 [ ! -f "/flagcounter/config/config.json" ]  && cp /flagcounter/config.json /flagcounter/config/
 
 node src/index.js --docker
